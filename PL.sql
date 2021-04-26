c3f3d785aec3352760317501ed930cbcec613a28
Felipe Nunes <fnap@cin.ufpe.br>
2021-04-26 11:50:48 -0300

    trigger: usuario cadastrado com sucesso

--- PL.sql
+++ PL.sql
@@ -1,1 +1,7 @@
-
+-- Printa que um usuario foi cadastrado com sucesso apos insercao
+CREATE OR REPLACE TRIGGER howmany
+AFTER INSERT
+ON Usuario
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Usuario cadastrado com sucesso');
+END;
