a751b57dbd851ec9c08994badc5bb647cf630a6b
Maria Luísa Lima <50404135+maluwastaken@users.noreply.github.com>
2021-04-26 12:03:27 -0300

    Update PL.sql

--- PL.sql
+++ PL.sql
@@ -5,3 +5,17 @@ ON Usuario
 BEGIN
     DBMS_OUTPUT.PUT_LINE('Usuario cadastrado com sucesso');
 END;
+
+CREATE OR REPLACE FUNCTION get_size(id_artista VARCHAR) 
+RETURN NUMBER
+IS
+    tmp varchar(80);
+    tmp2 number(10);
+BEGIN
+    -- pegar tamanho da bio do artista
+    select biografia into tmp from Artista where IDArtista = id_artista;
+    tmp2 := length(tmp);
+    DBMS_OUTPUT.PUT_LINE(tmp2);
+    return tmp2;
+END;
+
